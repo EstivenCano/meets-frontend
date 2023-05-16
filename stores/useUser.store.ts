1c8cbca44ae0277b7dcf0587bcdfd5f053837478
Estiven Cano <estivencano99@gmail.com>
2023-05-16 10:48:07 -0500

    [Add] Persistent user

--- stores/useUser.store.ts
+++ stores/useUser.store.ts
@@ -1,12 +1,20 @@
 import { User } from "@/model/User";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 interface userStore {
   user: User | null;
   setUser: (user: User | null) => void;
 }
 
-export const userStore = create<userStore>()((set) => ({
-  user: null,
-  setUser: (user) => set(() => ({ user })),
-}));
+export const userStore = create<userStore>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set(() => ({ user })),
+    }),
+    {
+      name: "user-storage",
+    }
+  )
+);
