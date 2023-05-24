3f4b3a9d0bc955469dae4633875f1d4334db7544
Estiven Cano <estivencano99@gmail.com>
2023-05-24 16:19:33 -0500

    [Update] Modal size

--- components/Surfaces/Modal.tsx
+++ components/Surfaces/Modal.tsx
@@ -44,7 +44,7 @@ export const Modal: FC<ModalProps> = ({
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               transition={{ duration: 0.2 }}
-              className={`fixed top-0 left-0 w-screen h-screen bg-black/50 z-40 flex justify-center items-center ${className}`}>
+              className={`fixed top-0 left-0 w-screen h-full bg-black/50 z-40 flex justify-center items-center ${className}`}>
               <div
                 ref={ref}
                 className='relative bg-background h-screen w-screen min-h-[250px] max-h-screen max-w-4xl md:h-fit md:w-full gap-y-4 md:rounded-xl flex flex-col items-start'>
