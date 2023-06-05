97645838d90add6123094c53d0ad84d1bc1dd6e0
Estiven Cano <estivencano99@gmail.com>
2023-06-05 16:13:43 -0500

    [Add] Initial animation for NoFeed component

--- components/Display/FeedList/NoFeed.tsx
+++ components/Display/FeedList/NoFeed.tsx
@@ -1,14 +1,22 @@
 import { useTranslation } from "@/app/i18n/client";
+import { motion } from "framer-motion";
 
 export const NoFeed = () => {
   const { t } = useTranslation("feed");
 
   return (
-    <div className='flex flex-col items-center justify-center w-full h-full gap-y-4'>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        duration: 1.5,
+        delay: 1,
+      }}
+      className='flex flex-col items-center justify-center w-full h-full gap-y-4'>
       <h2 className='text-xl font-bold'>
         {t("welcome")} <span className='text-violet-400'>Meets</span>
       </h2>
       <p className='text-sm text-gray-500 text-center'>{t("noFeed")}</p>
-    </div>
+    </motion.div>
   );
 };
